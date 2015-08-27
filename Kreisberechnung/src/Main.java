16c6a7de42c3f59b817b28011934d728b8a57160
Cralorwow <justdreamless@gmail.com>
2015-08-27 17:57:17 +0200

    Added Flaeche and Durchmesser
    -Added Flaeche and Durchmesser

--- Kreisberechnung/src/Main.java
+++ Kreisberechnung/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
-		double radius, umfang;
+		double radius, umfang, flaeche, durchmesser;
 		String einheit;
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,9 +21,15 @@ public class Main {
 		System.out.println("Bitte geben sie eine Einheit an. (mm/cm/dm/m");
 		einheit = br.readLine();
 		
+		durchmesser = radius * 2;
 		umfang = 2 * Math.PI * radius;
+		flaeche = Math.PI * (radius * radius);
 		
+		
+		System.out.println("Der von ihnen angegebene Kreis hat einen Durchmesser von: " + durchmesser + " " + einheit);
 		System.out.println("Der von ihnen angegebene Kreis hat einen Umfang von: " + umfang + " " + einheit);
+		System.out.println("Der von ihnen angegebene Kreis hat eine Flaeche von: " + flaeche + " " + einheit + "\u00b2");
+		
 	}
 
 }
